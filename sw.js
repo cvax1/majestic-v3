9aa209a2f68b46e068d107ec07de29b90a713596
cvax1 <dominikoposral@gmail.com>
2026-06-14 10:13:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,16 @@
 const CACHE = "majestic-v1";
-const FILES = ["/", "/МАДЖЕСТИК.html", "/manifest.json"];
+const FILES = ["/majestic-v3/", "/majestic-v3/index.html", "/majestic-v3/manifest.json"];
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
-
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
